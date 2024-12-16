d4fd41e9c33bf0f80f3056c1e5932e7287ebc9f4
Goutam Adhikari <gtm10322@gmail.com>
2024-12-16 22:14:58 +0600

    Activities Section Upload

--- myfedback/src/App.jsx
+++ myfedback/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Activities from './Components/Activities'
 import Carousel from './Components/Carousel'
 import Discover from './Components/Discover'
 import Footer from './Components/Footer'
 import Items from './Components/Items'
 import MyNav from './Components/MyNav'
 import Trends from './Components/Trands'
-import Card2 from './Components/Trands'
+
 
 function App() {
   
@@ -18,6 +19,7 @@ function App() {
     <Carousel/>
     <Trends/>
     <Discover/>
+    <Activities/>
     <Footer/>
     
 
