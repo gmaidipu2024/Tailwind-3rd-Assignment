49f9ca5732650ce711d14846f8ddb8b97870e9ee
Goutam Adhikari <gtm10322@gmail.com>
2024-12-16 14:42:37 +0600

    Carousel Update

--- myfedback/src/Components/Carousel.jsx
+++ myfedback/src/Components/Carousel.jsx
@@ -6,21 +6,27 @@ function Carousel() {
   const CarouselData = [
     {
       id: 1,
-      imgaes: "./bighand.png",
-      heading: "Wizkid made in lagos tour",
-      des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      imgaes: "./food1.png",
+      heading: "Bottega",
+      des: "The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.",
     },
     {
       id: 2,
-      imgaes: "./football.png",
-      heading: "Hablu Programmer",
-      des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      imgaes: "./rest1.png",
+      heading: "Bottega",
+      des: "The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.",
     },
     {
       id: 3,
-      imgaes: "./Bilding.png",
-      heading: "We are learning Front in development",
-      des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      imgaes: "./food2.png",
+      heading: "Bottega",
+      des: "The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.",
+    },
+    {
+      id: 3,
+      imgaes: "./rest2.png",
+      heading: "Bottega",
+      des: "The lorem ipsum is, in printing, a series of meaningless words used temporarily to calibrate a layout.",
     },
   ];
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -28,106 +34,175 @@ function Carousel() {
   const currentSlider = CarouselData[currentIndex];
 
   const handelPrev = () => {
-    setCurrentIndex((prevIndex)=>(
-      prevIndex === 0 ? CarouselData.length -1: prevIndex - 1
-    ))
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? CarouselData.length - 1 : prevIndex - 1
+    );
   };
-  const handelNext=()=>{
-    setCurrentIndex((prevIndex)=>(
-      prevIndex === CarouselData.length -1 ? 0 : prevIndex + 1
-    ))
+  const handelNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === CarouselData.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   const TagData = [
     {
       id: 1,
-      imgs: "./Calendar.png",
-      title: "Dec 12 2022",
+      imgs: "./star.png",
     },
     {
       id: 2,
-      imgs: "./Mark.png",
-      title: "TBS  Lagos",
+      imgs: "./star.png",
     },
     {
       id: 3,
-      imgs: "./Clock.png",
-      title: "9:00 PM ",
+      imgs: "./star.png",
     },
     {
       id: 4,
-      imgs: "./Tag.png",
-      title: "N20,000",
+      imgs: "./star.png",
+    },
+    {
+      id: 4,
+      imgs: "./star.png",
     },
   ];
 
   return (
     <section className="overflow-hidden  ">
-      <h1 className="text-center text-[#26395C] text-xl md:text-3xl lg:text-4xl font-bold py-4 mb-4">
-        See How People are Chilling On Chillsbay
-      </h1>
+      <div className="bg-[#1677BD] mx-auto container rounded-xl">
+        <div className=" relative w-full max-w-screen-2xl mx-auto overflow-hidden ">
+          <h1 className="text-start text-[white] text-xl md:text-3xl lg:text-4xl font-bold py-4 mb-4 ml-5">
+            Find the best restaurant ratings below
+          </h1>
 
-      <div className=" relative w-full max-w-screen-2xl mx-auto ">
-        <div className="flex items-center justify-between">
-          <button
-            onClick={handelPrev}
-            className=" absolute left-9 transform -translate-x-6 flex items-center justify-center bg-gray-600 text-white w-8 h-8 rounded-full "
-          >
-            <FaArrowLeft />
-          </button>
-          <div className="flex flex-col lg:flex-row items-center justify-center w-full px-12 gap-6">
-            <div className="w-full lg:w-1/2">
-              <h2 className="text-[#26395C] text-[15px] md:text-2xl lg:text-3xl xl:text-4xl font-semibold py-4">
-                {" "}
-                Upcoming events this weekend
-              </h2>
-              <img
-                className="object-cover w-full h-auto"
-                src={currentSlider.imgaes}
-                alt=""
-              />
-            </div>
-            {/* right site carousel */}
-            <div className="w-full lg:w-1/2">
-              <h2 className="text-md lg:text-5xl font-bold lg:w-[50%] text-start leading-tight py-5 mb-6">
-                {currentSlider.heading}
-              </h2>
-              <p className=" text-gray-700 text-justify lg:w-[70%] mb-10">
-                {" "}
-                {currentSlider.des}{" "}
-              </p>
+          <div className="flex items-center justify-between  ">
+            <button
+              onClick={handelPrev}
+              className=" absolute left-9 transform -translate-x-6 flex items-center justify-center bg-gray-600 text-white w-8 h-8 rounded-full "
+            >
+              <FaArrowLeft />
+            </button>
 
-              <div className="grid grid-cols-2 lg:w-[70%]">
-                {TagData.map((data, i) => (
-                  <div key={i} className="flex items-center gap-2  mb-5">
-                    <img src={data.imgs} alt="" />
-                    <p className=" text-xs lg:text-lg font-semibold">
-                      {data.title}
-                    </p>
-                  </div>
-                ))}
-              </div>
-              <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 lg:w-[60%]">
-                <Button color="primary" variant="bordered">
+            <div className="flex flex-col lg:flex-row items-center justify-center py-10  gap-6 px-4 ">
+              {/* div 1 */} 
+              <div className="w-fit lg:w-1/2 bg-white rounded-3xl mx-auto ">
+                <img className="w-full mx-auto" src={currentSlider.imgaes} alt="" />
+                
+                <h2 className="text-md lg:text-4xl font-bold lg:w-[50%] text-start leading-tight py-2 mb-2 ml-4">
+                  {currentSlider.heading}
+                </h2>
+                <p className=" text-gray-700 text-xl text-justify lg:w-[70%] mb-2 ml-5">
                   {" "}
-                  Add to cart
-                </Button>
-                <Button color="primary"> Book Now</Button>
-              </div>
-              <div className="flex gap-2 items-center mt-4 mb-5">
-                <p>Connect with us on:</p>
-                <img src="./Twitter.png" alt="" />
-                <img src="./Instagram.png" alt="" />
-              </div>
-            </div>
-          </div>
+                  {currentSlider.des}{" "}
+                </p>
 
-          <button
-            onClick={handelNext}
-            className=" absolute right-0 transform -translate-x-6 flex items-center justify-center bg-gray-700 text-white w-8 h-8 rounded-full "
-          >
-            <FaArrowRight />
-          </button>
+                <div className="flex  gap-2 mb-2 ml-4 ">
+                  
+                  {TagData.map((data, i) => (
+                    <div key={i} className="flex items-center gap-2 ">
+                      <img src={data.imgs} alt="" />
+                    </div>
+                  ))}
+                  
+                  <div className="py-4 ">
+                    <p> 5.0 (8/6 reviews)</p>
+                  </div>
+
+                </div>
+
+              </div>
+              
+              <div className="w-fit lg:w-1/2 bg-white rounded-3xl mx-auto ">
+                <img className="w-full mx-auto" src={currentSlider.imgaes} alt="" />
+                
+                <h2 className="text-md lg:text-4xl font-bold lg:w-[50%] text-start leading-tight py-2 mb-2 ml-4">
+                  {currentSlider.heading}
+                </h2>
+                <p className=" text-gray-700 text-xl text-justify lg:w-[70%] mb-2 ml-5">
+                  {" "}
+                  {currentSlider.des}{" "}
+                </p>
+
+                <div className="flex  gap-2 mb-2 ml-4 ">
+                  
+                  {TagData.map((data, i) => (
+                    <div key={i} className="flex items-center gap-2 ">
+                      <img src={data.imgs} alt="" />
+                    </div>
+                  ))}
+                  
+                  <div className="py-4 ">
+                    <p> 5.0 (8/6 reviews)</p>
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div className="w-fit lg:w-1/2 bg-white rounded-3xl mx-auto ">
+                <img className="w-full mx-auto" src={currentSlider.imgaes} alt="" />
+                
+                <h2 className="text-md lg:text-4xl font-bold lg:w-[50%] text-start leading-tight py-2 mb-2 ml-4">
+                  {currentSlider.heading}
+                </h2>
+                <p className=" text-gray-700 text-xl text-justify lg:w-[70%] mb-2 ml-5">
+                  {" "}
+                  {currentSlider.des}{" "}
+                </p>
+
+                <div className="flex  gap-2 mb-2 ml-4 ">
+                  
+                  {TagData.map((data, i) => (
+                    <div key={i} className="flex items-center gap-2 ">
+                      <img src={data.imgs} alt="" />
+                    </div>
+                  ))}
+                  
+                  <div className="py-4 ">
+                    <p> 5.0 (8/6 reviews)</p>
+                  </div>
+
+                </div>
+
+              </div>
+
+              <div className="w-fit lg:w-1/2 bg-white rounded-3xl mx-auto ">
+                <img className="w-full mx-auto" src={currentSlider.imgaes} alt="" />
+                
+                <h2 className="text-md lg:text-4xl font-bold lg:w-[50%] text-start leading-tight py-2 mb-2 ml-4">
+                  {currentSlider.heading}
+                </h2>
+                <p className=" text-gray-700 text-xl text-justify lg:w-[70%] mb-2 ml-5">
+                  {" "}
+                  {currentSlider.des}{" "}
+                </p>
+
+                <div className="flex  gap-2 mb-2 ml-4 ">
+                  
+                  {TagData.map((data, i) => (
+                    <div key={i} className="flex items-center gap-2 ">
+                      <img src={data.imgs} alt="" />
+                    </div>
+                  ))}
+                  
+                  <div className="py-4 ">
+                    <p> 5.0 (8/6 reviews)</p>
+                  </div>
+
+                </div>
+
+              </div>
+              
+
+            </div>
+
+            <button
+              onClick={handelNext}
+              className=" absolute right-0 transform -translate-x-6 flex items-center justify-center bg-gray-700 text-white w-8 h-8 rounded-full "
+            >
+              <FaArrowRight />
+            </button>
+          </div>
         </div>
       </div>
     </section>
