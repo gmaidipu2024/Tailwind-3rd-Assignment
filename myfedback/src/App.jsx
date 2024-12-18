fddb5158ab5cac6d1b4d873412a80c5e79a1f737
Goutam Adhikari <gtm10322@gmail.com>
2024-12-18 22:05:57 +0600

    vercel add

--- myfedback/src/App.jsx
+++ myfedback/src/App.jsx
@@ -1,14 +1,11 @@
 
 import './App.css'
 import Activities from './Components/Activities'
-import Carosl2 from './Components/Carosl2'
 import Carousel from './Components/Carousel'
 import Discover from './Components/Discover'
 import Footer from './Components/Footer'
 import Items from './Components/Items'
-import Map from './Components/Map'
 import MyNav from './Components/MyNav'
-import Page3 from './Components/Page3'
 import Trends from './Components/Trands'
 
 
